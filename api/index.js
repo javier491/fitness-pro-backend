80b0f273d7fc7dfcd555015087f4d9eae2bd717c
javier491 <83360903+javier491@users.noreply.github.com>
2026-05-30 17:06:34 -0600

    update

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const app = require('../src/app');
+const connectDB = require('../src/config/db');
+
+let connected = false;
+
+module.exports = async (req, res) => {
+  if (!connected) {
+    await connectDB();
+    connected = true;
+  }
+  return app(req, res);
+};
